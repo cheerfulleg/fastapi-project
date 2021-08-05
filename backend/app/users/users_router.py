cbdf9e05ed4c19077a7176d1deff6f5c09865872
ivanyolgin <ivan.yolgin@exrtawest.com>
2021-08-05 21:34:15 +0300

    changed tags

--- backend/app/users/users_router.py
+++ backend/app/users/users_router.py
@@ -5,5 +5,5 @@ from backend.app.users.api.user_views import user_router
 
 base_user_router = APIRouter()
 
-base_user_router.include_router(user_router, prefix='/user', tags=['Anonymous user action'])
-base_user_router.include_router(profile_router, prefix='/profile', tags=['Logged in user action'])
+base_user_router.include_router(user_router, prefix='/user', tags=['Anonymous user actions'])
+base_user_router.include_router(profile_router, prefix='/profile', tags=['Profile actions'])
